318952b60fe4344e3de8eb4ae9513b2254a5988a
Andrii Herchykov <andriiwp3@gmail.com>
2022-11-19 17:44:27 +0000

    feature: add swagger & port usage from env file

--- src/common/constants/env.constants.ts
+++ src/common/constants/env.constants.ts
@@ -1,5 +1,5 @@
 export const NODE_ENV = 'NODE_ENV';
-export const PORT = 'FT_PORT';
+export const APP_PORT = 'FT_PORT';
 export const DB_HOST = 'FT_DB_HOST';
 export const DB_PORT = 'FT_DB_PORT';
 export const DB_DBNAME = 'FT_DB_DBNAME';
